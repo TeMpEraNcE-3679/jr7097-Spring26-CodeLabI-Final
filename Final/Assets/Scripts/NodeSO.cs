35276dbf192557812ea112fdf9a325bca52f1c87
TeMpEraNcE-3679 <jiayuruan315@gmail.com>
2026-05-01 16:52:06 -0400

    updates

--- Final/Assets/Scripts/NodeSO.cs
+++ Final/Assets/Scripts/NodeSO.cs
@@ -5,11 +5,18 @@ public class NodeSO : ScriptableObject
 {
     [Header("Node Content")]
     public string NodeName;
+    public string NodeNumber;
     public Sprite Background;
     public Sprite Text;
 
     [Header("Choices")]
     public Choice[] choices;
+    
+    [Header("System Check")]
+    public int NumberofDays;
+    public int NumberofCans;
+    public int HungryDays;
+    public bool DidUEatMeat;
 }
 
 [System.Serializable]
